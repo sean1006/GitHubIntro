80e9c433178e3433d14de8e5819975f6bb9c97e1
Sean Yin <sean999_y@yahoo.com>
2018-12-15 21:41:13 -0800

    cleaned up

--- lolstat.py
+++ lolstat.py
@@ -6,27 +6,18 @@ from bs4 import BeautifulSoup
 
 
 def simple_get(url):
-    """
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None.
-    """
     try:
         with closing(get(url, stream=True)) as resp:
             if is_good_response(resp):
                 return resp.content
             else:
                 return None
-
     except RequestException as e:
         log_error('Error during requests to {0} : {1}'.format(url, str(e)))
         return None
 
 
 def is_good_response(resp):
-    """
-    Returns True if the response seems to be HTML, False otherwise.
-    """
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200
             and content_type is not None
@@ -34,19 +25,14 @@ def is_good_response(resp):
 
 
 def log_error(e):
-    """
-    It is always a good idea to log errors.
-    This function just prints them, but you can
-    make it do anything.
-    """
     print(e)
 
+    
 def main():
     website = "http://na.op.gg/summoner/champions/userName="
     user = raw_input()
     html_raw = simple_get(website+user)
     html = BeautifulSoup(html_raw, 'html.parser')
-    #f = open("kevostat.txt","w+")
     winRate = html.findAll("div", {"class": "WinRatioGraph"})
     champName = html.findAll("td", {"class": "ChampionName Cell"})
     i = 0
@@ -73,7 +59,7 @@ def main():
             j+=1
         print("%")
         i += 1
-    #f.close()
+
 
 if __name__ == '__main__':
     main()
