827ffead44f87451073c304adad730e762cadef3
Sean Yin <sean999_y@yahoo.com>
2021-01-26 14:02:32 -0800

    more openmp examples

--- C/quicksort_openmp.c
+++ C/quicksort_openmp.c
@@ -1,6 +1,7 @@
 // template Program in C using OpenMP
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <omp.h>
 #include <time.h>
 
@@ -76,15 +77,89 @@ void test_quicksort() {
     print_int_array(arr, LENGTH_ARRAY);
 }
 
+int HIST_INPUT_LEN = 999999;
+int HIST_NUM_BUCKETS = 20;
+// histogram counts the number of elements in each bucket
+void histogram(int *a, int *h) {
+    int N = HIST_INPUT_LEN;
+    int m = HIST_NUM_BUCKETS;
+    // initialize histogram by setting buckets to zero
+    #pragma omp parallel for
+    for (int i = 0; i < m; i++) {
+        h[i] = 0;
+    }
+    // populate histogram by number of appearences
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {
+        int index = a[i] - 1;
+        if (index >= 0 && index < N) {
+            #pragma omp atomic
+            h[index]++;
+        }
+    }
+}
+
+// test the histogram algorithm
+void test_histogram() {
+    int arr[HIST_INPUT_LEN];
+    int hist[HIST_NUM_BUCKETS];
+
+    // setup testing
+    initialize_random();
+    for (int i = 0; i < HIST_INPUT_LEN; i++) {
+        arr[i] = rand() % HIST_NUM_BUCKETS + 1;
+    }
+    print_int_array(arr, HIST_INPUT_LEN);
+
+    // create histgram
+    histogram(arr, hist);
+
+    // print results
+    print_int_array(hist, HIST_NUM_BUCKETS);
+}
+
+// integrate root(x) / (1 + x^3) between 0 and 1 using 16 threads NUMBER_1
+double integration_1() {
+    int accuracy = 100;
+    double width = 1.0/(double) accuracy;
+    double answer = 0.0;
+    double x = 0.0;
+    double step = 0.0;
+
+    #pragma omp parallel for num_threads(8) private(x, step)
+    for (int i = 0; i < accuracy; i++) {
+        x = width * (i + 0.5);
+        step = width * sqrt(x) / (1 + x * x * x);
+        #pragma omp atomic
+        answer += step;
+    }
+    
+    return answer;
+}
+
+// integrate root(x) / (1 + x^3) between 0 and 1 using 16 threads NUMBER_2
+double integration_2() {
+    int accuracy = 100;
+    double width = 1.0/(double) accuracy;
+    double answer = 0.0;
+    double x = 0.0;
+    double step = 0.0;
+
+    #pragma omp parallel for num_threads(8) reduction(+:answer) private(x, step)
+    for (int i = 0; i < accuracy; i++) {
+        x = width * (i + 0.5);
+        step = width * sqrt(x) / (1 + x * x * x);
+        answer += step;
+    }
+    
+    return answer;
+}
+
 int main()
 {
-    // test openmp functions
-    // #pragma omp parallel num_threads(1)
-    // openmp_info();
-
-    // test quicksort
-    // #pragma omp parallel
-    test_quicksort();
-
+    // test_quicksort();
+    // test_histogram();
+    printf("Integration 1 Result: %lf\n", integration_1());
+    printf("Integration 2 Result: %lf\n", integration_2());
     return 0;
 }
